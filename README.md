6993b81f88e5987e023b50197ad2b70e452785c4
Donald Pipowitch <pipo@senaeh.de>
2018-05-15 08:32:19 +0200

    upload first version

---
+++ README.md
@@ -0,0 +1,13 @@
+# `casey`
+
+> A simple command line tool to uppercase and lowercase strings.
+
+This `README.md` is written as a tutorial. If you're just interested in using this package, you'll find the usage information first. In the second part I'll explain how this package was created. This should be helpful, if you want to create a similar package or if you want to contribute to this package.
+
+## Usage
+
+TODO
+
+## Contribute
+
+TODO
---
+++ src/main.rs
@@ -0,0 +1,140 @@
+extern crate termion;
+
+use termion::cursor::{DetectCursorPos, Goto};
+use termion::event::Key;
+use termion::{clear, color, terminal_size};
+use termion::input::TermRead;
+use termion::raw::{IntoRawMode, RawTerminal};
+use std::io::{stdin, stdout, Write};
+
+#[derive(Debug)]
+struct State {
+    value: String,
+    index: usize,
+    start_row: usize,
+    rows: usize,
+}
+
+impl State {
+    fn new() -> State {
+        State {
+            value: String::new(),
+            index: 0,
+            start_row: 0,
+            rows: 1,
+        }
+    }
+}
+
+fn format_value(state: &State) -> String {
+    if state.value.is_empty() {
+        format!(
+            "{grey}Start typing...{reset}",
+            grey = color::Fg(color::LightBlack),
+            reset = color::Fg(color::Reset)
+        )
+    } else {
+        format!(
+            "{value}\n{grey}{uppercase}\n{lowercase}{reset}",
+            value = state.value,
+            grey = color::Fg(color::LightBlack),
+            uppercase = state.value.to_uppercase(),
+            lowercase = state.value.to_lowercase(),
+            reset = color::Fg(color::Reset),
+        )
+    }
+}
+
+fn render<W: Write>(stdout: &mut RawTerminal<W>, state: &mut State) {
+    // clear all "known" rows
+    for i in 0..state.rows {
+        write!(
+            stdout,
+            "{}{}",
+            Goto(1, state.start_row as u16 + i as u16),
+            clear::CurrentLine
+        ).unwrap();
+    }
+
+    // write
+    let formatted = format_value(&state);
+    for (i, line) in formatted.lines().enumerate() {
+        let (_total_cols, total_rows) = terminal_size().unwrap();
+        if state.start_row + i > total_rows as usize {
+            write!(stdout, "\n").unwrap();
+            state.start_row -= 1;
+            state.rows += 1;
+        }
+        write!(
+            stdout,
+            "{}{}",
+            Goto(1, state.start_row as u16 + i as u16),
+            line
+        ).unwrap();
+    }
+
+    // update cursor
+    write!(
+        stdout,
+        "{}",
+        Goto(state.index as u16 + 1, state.start_row as u16)
+    ).unwrap();
+
+    stdout.flush().unwrap();
+}
+
+fn main() {
+    let stdin = stdin();
+    let mut stdout = stdout().into_raw_mode().unwrap();
+
+    let (_col, start_row) = stdout.cursor_pos().unwrap();
+    let mut state = State::new();
+    state.start_row = start_row as usize;
+    render(&mut stdout, &mut state);
+
+    for key in stdin.keys() {
+        match key.unwrap() {
+            Key::Ctrl('c') => {
+                if !state.value.is_empty() {
+                    // jump to lower cased line, before exiting
+                    write!(stdout, "{}", Goto(1, state.start_row as u16 + 2)).unwrap();
+                }
+                break;
+            }
+            Key::Char('\n') => {
+                if state.value.is_empty() {
+                    break;
+                } else {
+                    write!(stdout, "{}\n", Goto(1, state.start_row as u16 + 2)).unwrap();
+                    stdout.flush().unwrap();
+                    state.value = String::new();
+                    state.index = 0;
+                    state.start_row += 2;
+                }
+            }
+            Key::Char(key) => {
+                state.value.insert(state.index, key);
+                state.index += 1;
+            }
+            // Key::Delete = entf
+            Key::Backspace => {
+                if !state.value.is_empty() && state.index != 0 {
+                    state.value.remove(state.index - 1);
+                    state.index -= 1;
+                }
+            }
+            Key::Left => {
+                if state.index > 0 {
+                    state.index -= 1;
+                }
+            }
+            Key::Right => {
+                if state.index < state.value.len() - 1 {
+                    state.index += 1;
+                }
+            }
+            _ => {}
+        }
+        render(&mut stdout, &mut state);
+    }
+}
